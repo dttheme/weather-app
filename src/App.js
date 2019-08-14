4e9a8b7bb9139ffe280a27ff7a385a6fc5215a2a
dttheme <dttheme@gmail.com>
2019-08-14 14:39:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import * as d3 from "d3";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const fetchWeatherAPI = async (lat, lon) => {
+  //   console.log(lat, lon);
+  //   await fetch(
+  //     `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${
+  //       process.env.REACT_APP_OPEN_WEATHER_API_KEY
+  //     }&units=imperial`
+  //   )
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       // setCityName(data.city.name);
+  //       // setGroupedData(groupByDayOfWeek(data.list));
+  //       console.log(data);
+  //     })
+  //     .catch(err => console.log(err));
+  // };
+
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     fetchWeatherAPI(position.coords.latitude, position.coords.longitude);
+  //   });
+  //   // fetchWeatherAPI(30312);
+  // }, []);
+
+  const drawChart = () => {
+    const data = [12, 5, 3, 7, 1, 21, 7];
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", 700)
+      .attr("height", 300);
+    svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect");
+  };
+  useEffect(() => {
+    drawChart();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
